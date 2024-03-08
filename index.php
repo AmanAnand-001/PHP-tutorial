416fca39e5b3a4ebf57d90f9feb7805e4badea46
Aman Anand <125989570+AmanAnand-001@users.noreply.github.com>
2024-03-08 20:34:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP tutorial</title>
+</head>
+<body>
+
+
+    <?php 
+    // Printing Hello world (Comment)
+    echo "Hello World !!" ?>
+
+    <?php $Variable1 = 12;
+     $Variable2 = 10;
+
+     echo $Variable1;
+     ?>
+
+</body>
+</html>
